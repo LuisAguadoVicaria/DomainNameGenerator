f800a70b10547fd2200c7ec7ac2326c167424ac0
LuisAguadoVicaria <70114505+LuisAguadoVicaria@users.noreply.github.com>
2022-06-10 16:44:39 +0200

    Simplify, clean and variable opt.

--- domain.js
+++ domain.js
@@ -1,29 +1,24 @@
-let pronouns = ['the','our', 'your', 'tu', 'my'];
-let adjs = ['great', 'big', 'tiny', 'small'];
-let nouns = ['jogger','racoon', 'puedes', 'turtle', 'bear', 'wolf'];
+let data = {}
+data.pronouns = ['the','our', 'your', 'tu', 'my']
+data.adjs = ['great', 'big', 'tiny', 'small']
+data.nouns = ['jogger','racoon', 'puedes', 'turtle', 'bear', 'wolf']
+data.domains = ['com', 'es', 'net', 'ru', 'ar']
+data.genDomains = []
+data.genHacks = []
 
-let domains = ['com', 'es', 'net', 'ru', 'ar']
-
-let genWords = []
-for(let pronoun of pronouns){
-	for(let adj of adjs){
-		for(let noun of nouns){
-			genWords.push(pronoun+adj+noun)
+for(let pronoun of data.pronouns){
+	for(let adj of data.adjs){
+		for(let noun of data.nouns){
+			for(let domain of data.domains){
+				let word = pronoun+adj+noun
+				data.genDomains.push(word+'.'+domain)
+				word = word.split("")
+				let shortw = word.splice(0, word.length-domain.length).join("")
+				if(word.join("")===domain)data.genHacks.push(shortw+'.'+domain)
+			}
 		}
 	}
 }
 
-let domainHacks = []
-const checkHack= (word, domain) => 
-domain == word.slice(word.length-domain.length, word.length+domain.length)
-? true : false
-
-const resultingDomains = genWords.map( (word) => { let out=[]
-  		domains.forEach( (domain) => {
-			out.push(word+'.'+domain)
-			if(checkHack(word, domain))domainHacks.push(word.slice(0, word.length-domain.length)+'.'+domain)
-  		} )
-  return out } ).flat()
-  
-console.log('Domains:',resultingDomains)
-console.log('Domain Hacks:',domainHacks)
+console.log('Domains:', data.genDomains)
+console.log('Domain Hacks:', data.genHacks)
